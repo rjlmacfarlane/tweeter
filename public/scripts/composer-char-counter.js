9d8ecf9718b93221bbaa29c5cca31c4adf6bea05
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-15 15:17:28 -0400

    Implemented character counter and input limiter (max 140 chars)

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $('textarea').on('input', function() {
+    const maxChars  = 140;
+    const charCount = $(this).val().length;
+    $(this).nextAll(".counter").text(maxChars - charCount);
+    
+    if (maxChars - charCount === 0) {
+      $(this).nextAll(".counter").css("color", "#c03333").text("Limit reached!");
+
+    } else {
+      $(this).nextAll(".counter").css("color", "#494b54");
+
+    }
+  });
+});
