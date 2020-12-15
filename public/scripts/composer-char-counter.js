76f59cf8818c96674c7ed6c993694e7ea3de174b
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-15 17:45:20 -0400

    Finalized tweet handler layout & styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 $(document).ready(function() {
 
   $('textarea').on('input', function() {
     const maxChars  = 140;
     const charCount = $(this).val().length;
     $(this).nextAll(".counter").text(maxChars - charCount);
-    
+
     if (maxChars - charCount === 0) {
       $(this).nextAll(".counter").css("color", "#c03333").text("Limit reached!");
 
