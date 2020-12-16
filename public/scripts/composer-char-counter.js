62fb35dc52b4e8d2c4614a175701f79aab7728e2
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-15 22:21:46 -0400

    Minor CSS style adjustments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
   $('textarea').on('input', function() {
     const maxChars  = 140;
     const charCount = $(this).val().length;
-    $(this).nextAll(".counter").text(maxChars - charCount);
 
     if (maxChars - charCount === 0) {
       $(this).nextAll(".counter").css("color", "#c03333").text("Limit reached!");
 
     } else {
       $(this).nextAll(".counter").css("color", "#494b54");
+      $(this).nextAll(".counter").text(maxChars - charCount);
 
     }
   });
