e3c95f899c501b9e88a32201da6d8a5a6910bec2
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-16 14:03:28 -0400

    Initial framework commit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
   $('textarea').on('input', function() {
     const maxChars  = 140;
     const charCount = $(this).val().length;
+    const $target = $(this).nextAll(".counter");
 
     if (maxChars - charCount === 0) {
-      $(this).nextAll(".counter").css("color", "#c03333").text("Limit reached!");
+      $target.css("color", "#c03333").text("Limit reached!");
 
     } else {
-      $(this).nextAll(".counter").css("color", "#494b54");
-      $(this).nextAll(".counter").text(maxChars - charCount);
+      $target.css("color", "#494b54");
+      $target.text(maxChars - charCount);
 
     }
   });
